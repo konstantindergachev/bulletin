32714d459e0f34cc9143bffd5b7faa4ebf938553
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-18 13:08:37 +0200

    Add Navbar.scss file to the Navbar component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,12 +3,16 @@ import Logo from '../logo/Logo';
 import Login from './LogIn';
 import Logout from './LogOut';
 
+import './Navbar.scss';
+
 const Navbar = () => {
   return (
     <React.Fragment>
-      <nav className="navbar"><Logo /></nav>
-      <Login />
-      <Logout />
+      <nav className="navbar">
+        <Logo />
+        <Login />
+        <Logout />
+      </nav>
     </React.Fragment>
   );
 };
