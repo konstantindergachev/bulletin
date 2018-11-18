2353ae5a0cd9cb90d7fc3086246c4b02c40a1ecc
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-18 12:51:39 +0200

    Add Footer component to the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import './App.scss';
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
     return (
       <div className="app">
         <Navbar />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    Copyright &copy; {new Date().getFullYear()} Bulletin
+  </footer>
+);
+export default Footer;
