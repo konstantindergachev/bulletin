f00fd2d10d9010a555a162715bfbc78e7bb53c72
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-18 13:03:03 +0200

    Add Logout component to the Navbar component

---
+++ src/components/layout/Logout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Logout = () => {
+  return (
+    <ul className="menu">
+      <li className="menu__item">
+        New Project
+      </li>
+      <li className="menu__item">
+        LogOut
+      </li>
+    </ul>
+  );
+};
+
+export default Logout;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Logo from '../logo/Logo';
 import Login from './LogIn';
+import Logout from './LogOut';
 
 const Navbar = () => {
   return (
     <React.Fragment>
       <nav className="navbar"><Logo /></nav>
       <Login />
+      <Logout />
     </React.Fragment>
   );
 };
