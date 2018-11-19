d4a9be6aeb2a534fc67d6a9c32ab5771a7bc74dc
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-19 08:51:11 +0200

    Create ui dir and TextFieldGroup component

---
+++ src/components/ui/TextFieldGroup.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextFieldGroup = ({ type, name, placeholder, value, onChange }) => {
+  return (
+    <div className="form__group">
+      <input
+        className="form__control"
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+TextFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+TextFieldGroup.defaultProps = {
+  type: 'text',
+};
+
+export default TextFieldGroup;
