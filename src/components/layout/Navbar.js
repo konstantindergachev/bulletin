5f9f8549d386e5b754366f19bfb2b96ae2991380
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-21 09:27:30 +0200

    Get authorization status from the firebase. Transfer it to the Logout component. Wrap Logo component to the Link component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Logo from '../logo/Logo';
 import Login from './LogIn';
 import Logout from './LogOut';
 
 import './Navbar.scss';
 
-const Navbar = () => {
+const Navbar = ({ authStatus, initials }) => {
+  console.log(`authStatus: `, authStatus);
+  console.log(`initials: `, initials);
   return (
-    <React.Fragment>
       <nav className="navbar">
-        <Logo />
-        <Login />
-        <Logout />
+        <Link to="/" className="logo__link">
+          <Logo />
+        </Link>
+        {!authStatus ? <Logout initials={initials} /> : <Login />}
       </nav>
-    </React.Fragment>
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => ({
+  authStatus: state.firebase.profile.isEmpty,
+  initials: state.firebase.profile.initials,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Navbar);
