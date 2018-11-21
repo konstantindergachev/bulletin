7a568aca1b65379ca86399a36146291135fdd7d3
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-21 09:43:01 +0200

    Use signOut action creator in Logout component. Wrap Logout component withRouter function to use history object. Create initials list item element

--- src/components/layout/Logout.js
+++ src/components/layout/Logout.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { signOut } from '../../actions/authActions';
+import { firebaseConnect } from 'react-redux-firebase';
 
-const Logout = () => {
+const Logout = ({ signOut, firebase, history,  initials }) => {
   return (
     <ul className="menu">
       <li className="menu__item">
-        <Link to="/create" className="menu__item">New Project</Link>
+        <Link to="/create" className="menu__item">
+          New Project
+        </Link>
       </li>
       <li className="menu__item">
-        LogOut
+        <a className="menu__link" onClick={() => signOut(firebase, history)}>
+          LogOut
+        </a>
+      </li>
+      <li className="menu__item">
+        <Link className="menu__link" to="/" className="btn btn__circle">
+          {initials}
+        </Link>
       </li>
     </ul>
   );
 };
 
-export default Logout;
+export default compose(
+  firebaseConnect(),
+  connect(
+    null,
+    { signOut },
+  ),
+)(withRouter(Logout));
