a8d5aead695b8b1bab71e8ee36fd03809401b784
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-22 09:09:19 +0200

    Import ProjectSummary component to ProjectList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
+import { Link } from 'react-router-dom';
 
 const ProjectList = ({ projects }) => {
-    console.log(`projects: `, projects);
-  return <section className="project__wrapper">Project List</section>;
+  console.log(`projects: `, projects);
+  return (
+    <React.Fragment>
+      {projects &&
+        projects.map(project => (
+          <Link
+            className="project__wrapper"
+            key={project.id}
+            to={`/project/${project.id}`}
+          >
+            <ProjectSummary
+              title={project.title}
+              content={project.content}
+              authorFirstName={project.authorFirstName}
+              authorLastName={project.authorLastName}
+              // createdAt={project.createdAt}
+            />
+          </Link>
+        ))}
+    </React.Fragment>
+  );
 };
 export default ProjectList;
