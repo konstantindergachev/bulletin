c1109271a439ec1712ff0c4ea6f3953ad4a5ca9a
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-22 09:04:50 +0200

    Create ProjectSummary component

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProjectSummary = ({
+  title,
+  content,
+  authorFirstName,
+  authorLastName,
+  createdAt,
+}) => {
+  return (
+    <div className="project">
+      <span className="project__title">{title}</span>
+      <p className="project__content">{content}</p>
+      <div className="project__author">
+        <span>Posted by {authorFirstName} </span>
+        <span>{authorLastName}</span>
+      </div>
+      <p className="project__time">{createdAt}</p>
+    </div>
+  );
+};
+export default ProjectSummary;
