b1519b3bf4f40b276d68c9c430e2ef5ac36be260
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-23 09:10:58 +0200

    Add TYPES: CREATE_PROJECT and CREATE_PROJECT_ERROR. Add a createProject action creator

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,39 @@
 import { TYPES } from './types';
 
+//Create a project
+export const createProject = (
+  projectData,
+  uid,
+  profile,
+  firestore,
+  history,
+) => async dispatch => {
+  try {
+    dispatch(setProjectLoading());
+    const { firstName, lastName } = profile;
+    const project = await firestore.add(
+      { collection: 'projects' },
+      {
+        ...projectData,
+        authorFirstName: firstName,
+        authorLastName: lastName,
+        authorId: uid,
+        createdAt: new Date(),
+      },
+    );
+    dispatch({
+      type: TYPES.CREATE_PROJECT,
+      payload: project,
+    });
+    history.push('/');
+  } catch (err) {
+    dispatch({
+      type: TYPES.CREATE_PROJECT_ERROR,
+      payload: err,
+    });
+  }
+};
+
 //Get all projects
 export const getProjects = () => async dispatch => {
   try {
