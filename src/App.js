b8c6532b1e47e26ccce364d539c41906b78ea23f
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-23 09:15:24 +0200

    Create CreateProject commponent. Add CreateProject component to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import SignUp from './components/auth/SignUp';
 import SignIn from './components/auth/SignIn';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import CreateProject from './components/projects/CreateProject';
 import './App.scss';
 
 class App extends React.Component {
@@ -23,6 +24,7 @@ class App extends React.Component {
             <Route path="/signup" component={SignUp} />
             <Route path="/signin" component={SignIn} />
             <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/create" component={CreateProject} />
           </Switch>
           <Footer />
         </div>
---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+
+import { createProject } from '../../actions/projectActions';
+import TextFieldGroup from '../ui/TextFieldGroup';
+import TextAreaFieldGroup from '../ui/TextAreaFieldGroup';
+
+class CreateProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+    };
+  }
+
+  handleOnChange = ev => {
+    this.setState({ [ev.target.name]: ev.target.value });
+  }
+
+  handleSubmit = ev => {
+    ev.preventDefault();
+    const { title, content } = this.state;
+
+    const newProject = {
+      title: title,
+      content: content,
+    };
+    const {
+      auth: { uid },
+      profile,
+      firestore,
+      history,
+    } = this.props;
+    this.props.createProject(newProject, uid, profile, firestore, history);
+  }
+
+  render() {
+    const { auth } = this.props;
+    if (auth.uid) {
+      return (
+        <section className="create__wrapper">
+          <form className="create" onSubmit={this.handleSubmit}>
+            <h5 className="create__title">Create new project</h5>
+            <TextFieldGroup
+              name="title"
+              placeholder="title"
+              value={this.state.title}
+              onChange={this.handleOnChange}
+            />
+            <TextAreaFieldGroup
+              name="content"
+              placeholder="project content"
+              value={this.state.content}
+              onChange={this.handleOnChange}
+
+            />
+            <button className="btn btn__create">Create</button>
+          </form>
+        </section>
+      );
+    } else {
+      return <Redirect to="/signin" />;
+    }
+  }
+}
+
+const mapStateToProps = state => ({
+  profile: state.firebase.profile,
+  auth: state.firebase.auth,
+});
+
+export default compose(
+  firestoreConnect(),
+  connect(
+    mapStateToProps,
+    { createProject },
+  ),
+)(CreateProject);
