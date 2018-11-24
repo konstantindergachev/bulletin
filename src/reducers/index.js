088fee8b79973487eb6d81ea7abd008ab7a5df21
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-24 10:22:18 +0200

    Add projectReducer to combineReducer (reducers/index.js)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,11 @@ import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 import authReducer from './authReducer';
+import projectReducer from './projectReducer';
 
 export default combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
   auth: authReducer,
+  project: projectReducer,
 });
