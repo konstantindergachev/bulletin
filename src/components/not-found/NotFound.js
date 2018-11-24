2503153bd762e8f06d2fec31c73032461264d99e
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-24 10:28:48 +0200

    Create NotFound component

---
+++ src/components/not-found/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="page__not-found">
+            <h1 className="error__text">404 Page</h1>
+            <p>Sorry. Page that you looking for does'n exist</p>
+        </div>
+    );
+};
+export default NotFound;
