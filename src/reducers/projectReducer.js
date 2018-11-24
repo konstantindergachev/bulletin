1305e6e17ff41c2574c98169ecd255f3a2990c10
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-24 10:04:21 +0200

    Create projectReducer.js file with PROJECT_LOADING and GET_PROJECTS types

---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,25 @@
+import { TYPES } from '../actions/types';
+
+const initialState = {
+  projects: [],
+  loading: false,
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case TYPES.PROJECT_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case TYPES.GET_PROJECTS:
+      return {
+        ...state,
+        // projects: action.payload,
+        projects: state.projects,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
