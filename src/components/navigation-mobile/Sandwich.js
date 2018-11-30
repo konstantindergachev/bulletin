c45bf6b737b21c66e8bf2c866ce557adb34ac187
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2018-11-30 09:48:02 +0200

    Create Sandwich component and Sandwich.scss file

---
+++ src/components/navigation-mobile/Sandwich.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Sandwich.scss';
+
+const Sandwich = () => {
+  return (
+    <div onClick="sandwich">
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+export default Sandwich;
